dd2f7e3bb6d2aa9a98d0c0ea545024a6ff96f120
Rafael Alfaro <rafaela@thehangar.cr>
2018-02-20 22:54:07 -0600

    Logs Up Arrow Pressed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+	public float speed = 5.0f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +13,8 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKey(KeyCode.UpArrow)){
+			Debug.Log("UpArrow Pressed");
+		}
 	}
 }
