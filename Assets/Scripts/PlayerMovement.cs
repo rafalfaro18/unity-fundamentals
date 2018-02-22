1dd51070261b0e52aa54c6379d9fc77e600e0a94
Rafael Alfaro <rafaela@thehangar.cr>
2018-02-21 22:18:27 -0600

    refactor speed name

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	public float speed = 5.0f;
+	public float moveSpeed = 5.0f;
 	public float rotateSpeed = 100.0f;
 
 	// Use this for initialization
@@ -30,26 +30,26 @@ public class PlayerMovement : MonoBehaviour {
 		
 		if (Input.GetKey (KeyCode.UpArrow)) {
 			Debug.Log ("UpArrow Pressed");
-			this.transform.Translate (new Vector3 (0, 0, speed * Time.deltaTime));
+			this.transform.Translate (new Vector3 (0, 0, moveSpeed * Time.deltaTime));
 		} else if (Input.GetKey (KeyCode.DownArrow)) {
 			Debug.Log ("DownArrow Pressed");
-			this.transform.Translate (new Vector3 (0, 0, -speed * Time.deltaTime));
+			this.transform.Translate (new Vector3 (0, 0, -moveSpeed * Time.deltaTime));
 		}
 
 		if (Input.GetKey (KeyCode.LeftArrow)) {
 			Debug.Log ("LeftArrow Pressed");
-			this.transform.Translate (new Vector3 (-speed * Time.deltaTime, 0, 0));
+			this.transform.Translate (new Vector3 (-moveSpeed * Time.deltaTime, 0, 0));
 		} else if (Input.GetKey (KeyCode.RightArrow)) {
 			Debug.Log ("RightArrow Pressed");
-			this.transform.Translate (new Vector3 (speed * Time.deltaTime, 0, 0));
+			this.transform.Translate (new Vector3 (moveSpeed * Time.deltaTime, 0, 0));
 		}
 
 		if (Input.GetKey (KeyCode.W)) {
 			Debug.Log ("W Pressed");
-			this.transform.Translate (new Vector3 (0, speed * Time.deltaTime, 0));
+			this.transform.Translate (new Vector3 (0, moveSpeed * Time.deltaTime, 0));
 		} else if (Input.GetKey (KeyCode.S)) {
 			Debug.Log ("S Pressed");
-			this.transform.Translate (new Vector3 (0, -speed * Time.deltaTime, 0));
+			this.transform.Translate (new Vector3 (0, -moveSpeed * Time.deltaTime, 0));
 		}
 	}
 }
