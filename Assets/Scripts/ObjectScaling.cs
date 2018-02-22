05b2c6327a3736fa0001e15c6d04363939ad2ebe
Rafael Alfaro <rafaela@thehangar.cr>
2018-02-21 23:03:49 -0600

    grows on g key shrinks when released

---
+++ Assets/Scripts/ObjectScaling.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectScaling : MonoBehaviour {
+
+	public float scaleAmount = 3.0f;
+	public Vector3 originalScale = Vector3.zero;
+
+	void Awake(){
+		originalScale = this.transform.localScale;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.G)){
+			this.transform.localScale = new Vector3(scaleAmount, scaleAmount, scaleAmount);
+			//this.transform.localScale = Vector3.one * scaleAmount; //same as above
+		} else if(Input.GetKeyUp(KeyCode.G)){
+			this.transform.localScale = originalScale;
+		}
+
+	}
+}
