f996ffe4e1e0ecb807e3ee0847ac208b77701d65
Rafael Alfaro <rafaela@thehangar.cr>
2018-02-22 21:16:45 -0600

    adds wall material and script to change color

---
+++ Assets/Scripts/ObjectColor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectColor : MonoBehaviour {
+
+	public Color changedColor = Color.white;
+	public Color originalColor = Color.white;
+
+	void Awake (){
+		originalColor = this.renderer.material.color;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.G)) {
+			this.renderer.material.color = changedColor;
+		} else if (Input.GetKeyUp (KeyCode.G)) {
+			this.renderer.material.color = originalColor;
+		}
+	}
+}
