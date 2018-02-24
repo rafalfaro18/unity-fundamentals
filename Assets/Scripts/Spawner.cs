60866f36112a356ca8017d6da4481bbc0d56422c
Rafael Alfaro <rafaela@thehangar.cr>
2018-02-24 01:53:39 -0600

    spawns spheres when pressing space

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject spawnType = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			GameObject obj = Instantiate (spawnType) as GameObject;
+		}
+	}
+}
