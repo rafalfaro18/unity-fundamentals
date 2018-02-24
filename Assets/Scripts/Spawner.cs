0fb331a8133eaa742dd6f83eb9ed9fcf6d923145
Rafael Alfaro <rafaela@thehangar.cr>
2018-02-24 02:14:58 -0600

    randomizes spawned sphere size

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Spawner : MonoBehaviour {
 
 	public GameObject spawnType = null;
+	public float scaleMin = 0.2f;
+	public float scaleMax = 2.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -17,6 +19,8 @@ public class Spawner : MonoBehaviour {
 			GameObject obj = Instantiate (spawnType) as GameObject;
 			obj.transform.position = this.transform.position;
 			obj.AddComponent<Rigidbody> ();
+			float scaleSize = Random.Range (scaleMin, scaleMax);
+			obj.transform.localScale = Vector3.one * scaleSize;
 			obj.name = "mySphere";
 		}
 	}
