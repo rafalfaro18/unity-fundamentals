d396926a2ac1ae7e19149c693d1bb950d7baa95c
Rafael Alfaro <rafaela@thehangar.cr>
2018-02-24 02:17:53 -0600

    adds force back to spheres that spawn

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -18,9 +18,10 @@ public class Spawner : MonoBehaviour {
 		if (Input.GetKeyDown (KeyCode.Space)) {
 			GameObject obj = Instantiate (spawnType) as GameObject;
 			obj.transform.position = this.transform.position;
-			obj.AddComponent<Rigidbody> ();
 			float scaleSize = Random.Range (scaleMin, scaleMax);
 			obj.transform.localScale = Vector3.one * scaleSize;
+			obj.AddComponent<Rigidbody> ();
+			obj.rigidbody.AddRelativeForce (Vector3.back * 100);
 			obj.name = "mySphere";
 		}
 	}
