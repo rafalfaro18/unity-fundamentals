9d318a57a25e4e723d5c904d9b61298c9df7cbb4
Rafael Alfaro <rafaela@thehangar.cr>
2018-02-25 23:06:52 -0600

    refactor camera code

--- Assets/Scripts/Cameras.cs
+++ Assets/Scripts/Cameras.cs
@@ -9,25 +9,44 @@ public class Cameras : MonoBehaviour {
 	public Transform target = null; 
 	public Vector3 speed = new Vector3(4.0f, 2.0f, 1.0f);
 	public Vector3 nextPosition = Vector3.zero;
+	public enum CameraState { none, followPosition, lookAtPLayer, both};
+	public CameraState cameraState = CameraState.none;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
 	void LateUpdate () {
+		switch (cameraState) {
+			case CameraState.none:
+				break;
+			case CameraState.followPosition:
+				FollowPosition ();
+				break;
+			case CameraState.lookAtPLayer:
+				LookAtPLayer ();
+				break;
+			case CameraState.both:
+				FollowPosition ();
+				LookAtPLayer ();
+				break;
+		}
+	}
 
+	void FollowPosition(){
 		nextPosition.x = Mathf.Lerp (this.transform.position.x , target.position.x, speed.x * Time.deltaTime);
 		nextPosition.y = Mathf.Lerp (this.transform.position.y , target.position.y, speed.y * Time.deltaTime);
 		nextPosition.z = Mathf.Lerp (this.transform.position.z , target.position.z, speed.z * Time.deltaTime);
 		this.transform.position = nextPosition;
-		//this.transform.position = target.position;
-		this.transform.LookAt (player.position);
+	}
 
+	void LookAtPLayer(){
+		this.transform.LookAt (player.position);
 	}
 }
