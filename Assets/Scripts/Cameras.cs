0c726046b7702152227549502dfdc41fc1ef8a08
Rafael Alfaro <rafaela@thehangar.cr>
2018-02-25 22:52:14 -0600

    smooth camera follow

--- Assets/Scripts/Cameras.cs
+++ Assets/Scripts/Cameras.cs
@@ -7,7 +7,7 @@ public class Cameras : MonoBehaviour {
 
 	public Transform player = null;
 	public Transform target = null; 
-	public float speed = 5.0f;
+	public Vector3 speed = new Vector3(4.0f, 2.0f, 1.0f);
 	public Vector3 nextPosition = Vector3.zero;
 
 	// Use this for initialization
@@ -22,9 +22,9 @@ public class Cameras : MonoBehaviour {
 
 	void LateUpdate () {
 
-		nextPosition.x = Mathf.Lerp (this.transform.position.x , target.position.x, speed * Time.deltaTime);
-		nextPosition.y = Mathf.Lerp (this.transform.position.y , target.position.y, speed * Time.deltaTime);
-		nextPosition.z = Mathf.Lerp (this.transform.position.z , target.position.z, speed * Time.deltaTime);
+		nextPosition.x = Mathf.Lerp (this.transform.position.x , target.position.x, speed.x * Time.deltaTime);
+		nextPosition.y = Mathf.Lerp (this.transform.position.y , target.position.y, speed.y * Time.deltaTime);
+		nextPosition.z = Mathf.Lerp (this.transform.position.z , target.position.z, speed.z * Time.deltaTime);
 		this.transform.position = nextPosition;
 		//this.transform.position = target.position;
 		this.transform.LookAt (player.position);
