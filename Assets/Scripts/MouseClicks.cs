9adc3c4851e2fb356e4fcb5fd46576e99083bf53
Rafael Alfaro <rafaela@thehangar.cr>
2018-02-26 09:51:21 -0600

    changes color of cube on hovering it

--- Assets/Scripts/MouseClicks.cs
+++ Assets/Scripts/MouseClicks.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class MouseClicks : MonoBehaviour {
 
+	private Color originalColor = Color.white;
+
+	void Awake(){
+		originalColor = this.GetComponent<Renderer>().material.color;
+	}
+
+	void OnMouseEnter (){
+		this.GetComponent<Renderer>().material.color = Color.red;
+	}
+
+	void OnMouseExit (){
+		this.GetComponent<Renderer>().material.color = originalColor;
+	}
+
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		
