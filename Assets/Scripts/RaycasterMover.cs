c149984136173881125bbd0d98158e7cc6407e37
Rafael Alfaro <rafaela@thehangar.cr>
2018-02-26 22:57:34 -0600

    moves cube to clicked point

--- Assets/Scripts/RaycasterMover.cs
+++ Assets/Scripts/RaycasterMover.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class RaycasterMover : MonoBehaviour {
 
+
+	public Camera camera;
+	private Ray ray;
+	private RaycastHit hit;
+	private GameObject hitObject = null;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +17,25 @@ public class RaycasterMover : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		//Get Object
+		if(Input.GetMouseButtonDown(0)){
+			ray = camera.ScreenPointToRay (Input.mousePosition);
+			if(Physics.Raycast(ray, out hit)){
+				if(hit.collider.name == "Cube"){
+					hitObject = hit.collider.gameObject;
+				}
+			}
+		}
+
+		if(hitObject){
+			//Hold Object
+			if(Input.GetMouseButton(0)){
+				ray = camera.ScreenPointToRay (Input.mousePosition);
+				if (Physics.Raycast (ray, out hit)) {
+					hitObject.transform.position = new Vector3 (hit.point.x, hitObject.transform.position.y , hit.point.z);
+				}
+			}
+			//Drop Object
+		}
 	}
 }
