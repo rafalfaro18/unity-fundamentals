2ed62f895df7bfdc14aa88ee9549f39fc1c27c33
Rafael Alfaro <rafaela@thehangar.cr>
2018-02-26 20:20:36 -0600

    Adds Raycaster script to Raycaster object

--- Assets/Scripts/Raycaster.cs
+++ Assets/Scripts/Raycaster.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Raycaster : MonoBehaviour {
 
+	public Camera camera;
+
+	private Ray ray;
+	private RaycastHit hit;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +16,11 @@ public class Raycaster : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetMouseButtonDown(0)){
+			ray = camera.ScreenPointToRay (Input.mousePosition);
+			if(Physics.Raycast(ray)){
+				Debug.Log ("Hit Something");
+			}
+		}
 	}
 }
