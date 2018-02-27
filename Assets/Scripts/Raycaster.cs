3cf78d816f13917e17ba98e8877d02f15c1f7b08
Rafael Alfaro <rafaela@thehangar.cr>
2018-02-26 21:37:21 -0600

    raycasts from cube not camera

--- Assets/Scripts/Raycaster.cs
+++ Assets/Scripts/Raycaster.cs
@@ -9,6 +9,9 @@ public class Raycaster : MonoBehaviour {
 	private Ray ray;
 	private RaycastHit hit;
 
+	public Transform origin;
+	public float rayLength = 2;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -17,22 +20,36 @@ public class Raycaster : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown(0)){
-			ray = camera.ScreenPointToRay (Input.mousePosition);
+//			ray = camera.ScreenPointToRay (Input.mousePosition);
+
 //			if(Physics.Raycast(ray, out hit )){
 //				if(hit.collider){
 //					Debug.Log ("Hit " + hit.collider.name);
 //					Instantiate(obj, hit.point, hit.transform.rotation);
 //				}
 //			}
+
+//			RaycastHit[] hits = null;
+//			hits = Physics.RaycastAll (ray);
+//			if(hits.Length > 0){
+//				for(int i=0; i<hits.Length; i++){
+//					RaycastHit hit = hits[i];
+//					Debug.Log ("Hit " + hit.collider.name);
+//				}
+//			}
+
 			RaycastHit[] hits = null;
-			hits = Physics.RaycastAll (ray);
-			if(hits.Length > 0){
-				for(int i=0; i<hits.Length; i++){
+			hits = Physics.RaycastAll (origin.position, Vector3.forward, rayLength);
+			if (hits.Length > 0) {
+				for (int i = 0; i < hits.Length; i++) {
 					RaycastHit hit = hits[i];
-					Debug.Log ("Hit " + hit.collider.name);
+					if(hit.collider){
+						Debug.Log ("Hit " + hit.collider.name);
+					}
 				}
 			}
+			Debug.DrawRay (origin.position, Vector3.forward * rayLength, Color.red, 3);
 		}
-		Debug.DrawLine (ray.origin, hit.point, Color.red);
+//		Debug.DrawLine (ray.origin, hit.point, Color.red);
 	}
 }
