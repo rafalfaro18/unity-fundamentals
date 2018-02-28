7d7ba80cd49192fb1bd3f5c70c25960c1c4df407
Rafael Alfaro <rafaela@thehangar.cr>
2018-02-27 23:42:28 -0600

    Adds Example 11 moves cube to where mouse clicks

---
+++ Assets/Scripts/ClickAndMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickAndMove : MonoBehaviour {
+
+	public Camera camera;
+	public Transform player;
+	public Vector3 moveToPosition;
+	public float speed = 5;
+	private Ray ray;
+	private RaycastHit hit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			ray = camera.ScreenPointToRay (Input.mousePosition);
+
+			if(Physics.Raycast(ray, out hit)){
+				if(hit.collider.name == "Plane"){
+					moveToPosition = hit.point;
+
+				}
+			}
+		}
+
+		player.transform.position = Vector3.Lerp (player.transform.position, moveToPosition, speed * Time.deltaTime);
+	}
+}
