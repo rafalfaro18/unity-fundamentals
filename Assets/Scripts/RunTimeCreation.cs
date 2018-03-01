2974aa89fd8f08d68ee52de565e7235264afb30a
Rafael Alfaro <rafaela@thehangar.cr>
2018-02-28 21:35:53 -0600

    creates custom shape (triangle) on runtime

--- Assets/Scripts/RunTimeCreation.cs
+++ Assets/Scripts/RunTimeCreation.cs
@@ -16,10 +16,37 @@ public class RunTimeCreation : MonoBehaviour {
 		if(Input.GetKeyDown(KeyCode.A)){
 			CreatePrimitiveCube ();
 		}
+		if (Input.GetKeyDown (KeyCode.S)) {
+			CreateTriangle ();
+		}
 	}
 
 	void CreatePrimitiveCube(){
 		GameObject obj = GameObject.CreatePrimitive (primitiveType);
-		obj.AddComponent<Rigidbody> ();
+		obj.AddComponent<Rigidbody> ();   
+	}
+
+	void CreateTriangle(){
+		GameObject obj = new GameObject ();
+		obj.name = "Runtime created gameObject";
+
+		MeshFilter meshFilter = obj.AddComponent<MeshFilter> ();
+		Mesh mesh = meshFilter.mesh;
+
+		mesh.Clear ();
+		mesh.vertices = new Vector3[]{ new Vector3(0,0,0), new Vector3(0,1,0), new Vector3(1,1,0)};
+		mesh.uv = new Vector2[]{new Vector2(0,0), new Vector2(0,1), new Vector2(1,1)};
+		mesh.triangles = new int[] { 0,1,2};
+		mesh.RecalculateNormals ();
+		mesh.RecalculateBounds ();
+
+		MeshRenderer meshRenderer = obj.AddComponent<MeshRenderer> ();
+		Material material = new Material (Shader.Find("Diffuse"));
+		material.color = Color.blue;
+		meshRenderer.material = material;
+
+		MeshCollider meshCollider = obj.AddComponent<MeshCollider> ();
+		obj.layer = 0;
+		obj.tag = "Player";
 	}
 }
