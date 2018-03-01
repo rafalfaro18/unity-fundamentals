aa0d801f0c25bbcc325ca28aaee193c156660b67
Rafael Alfaro <rafaela@thehangar.cr>
2018-02-28 21:15:44 -0600

    creates cubes on runtime when pressing a

--- Assets/Scripts/RunTimeCreation.cs
+++ Assets/Scripts/RunTimeCreation.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class RunTimeCreation : MonoBehaviour {
 
+	public PrimitiveType primitiveType = PrimitiveType.Cube;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +13,13 @@ public class RunTimeCreation : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetKeyDown(KeyCode.A)){
+			CreatePrimitiveCube ();
+		}
+	}
+
+	void CreatePrimitiveCube(){
+		GameObject obj = GameObject.CreatePrimitive (primitiveType);
+		obj.AddComponent<Rigidbody> ();
 	}
 }
